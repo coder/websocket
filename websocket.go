22638e0bfb78969034c479c62cab5d63bd504b63
Anmol Sethi <hi@nhooyr.io>
2019-06-05 09:38:45 -0400

    Document separate idle and read timeout
    Closes #87

--- websocket.go
+++ websocket.go
@@ -342,6 +342,11 @@ func (c *Conn) handleControl(h header) {
 // be read if you do not read the message from the connection.
 //
 // Only one Reader may be open at a time.
+//
+// If you need a separate timeout on the Reader call and then the message
+// Read, use time.AfterFunc to cancel the context passed in early.
+// See https://github.com/nhooyr/websocket/issues/87#issue-451703332
+// Most users should not need this.
 func (c *Conn) Reader(ctx context.Context) (MessageType, io.Reader, error) {
 	typ, r, err := c.reader(ctx)
 	if err != nil {
