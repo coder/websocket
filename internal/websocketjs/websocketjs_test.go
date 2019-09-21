f6a26896c967a2f6e782f1e4a216204cebaa1507
Anmol Sethi <hi@nhooyr.io>
2019-09-20 22:18:29 -0500

    Implement low level typed interface to the browser WebSocket API
    Updates #121

---
+++ internal/websocketjs/websocketjs_test.go
@@ -0,0 +1,26 @@
+// +build js
+
+package websocketjs
+
+import (
+	"context"
+	"syscall/js"
+	"testing"
+	"time"
+)
+
+func TestWebSocket(t *testing.T) {
+	t.Parallel()
+
+	c, err := New(context.Background(), "ws://localhost:8081", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	c.OnError(func(e js.Value) {
+		t.Log(js.Global().Get("JSON").Call("stringify", e))
+		t.Log(c.v.Get("readyState"))
+	})
+
+	time.Sleep(time.Second)
+}
