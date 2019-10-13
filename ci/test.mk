d85d44f2ebe1adb292950e2f0220c6d82ccab8cc
Anmol Sethi <hi@nhooyr.io>
2019-10-13 18:56:09 -0400

    Remove separate script for wasm tests

--- ci/test.mk
+++ ci/test.mk
@@ -1,4 +1,4 @@
-test: gotest gotest-wasm
+test: gotest
 
 gotest: _gotest htmlcov
 ifdef CI
@@ -12,15 +12,8 @@ codecov: _gotest
 	curl -s https://codecov.io/bash | bash -s -- -Z -f ci/out/coverage.prof
 
 _gotest:
-	echo "--- gotest" && go test -parallel=32 -coverprofile=ci/out/coverage.prof -coverpkg=./... $$TESTFLAGS ./...
+	go test -parallel=32 -coverprofile=ci/out/coverage.prof -coverpkg=./... $$TESTFLAGS ./...
 	sed -i '/_stringer\.go/d' ci/out/coverage.prof
-	sed -i '/wsjstest\/main\.go/d' ci/out/coverage.prof
 	sed -i '/wsecho\.go/d' ci/out/coverage.prof
 	sed -i '/assert\.go/d' ci/out/coverage.prof
 	sed -i '/wsgrace\.go/d' ci/out/coverage.prof
-
-gotest-wasm: wsjstest
-	echo "--- wsjstest" && ./ci/wasmtest.sh
-
-wsjstest:
-	go install ./internal/wsjstest
--- conn_test.go
+++ conn_test.go
@@ -2337,3 +2337,38 @@ func checkWSTestIndex(t *testing.T, path string) {
 		}
 	}
 }
+
+func TestWASM(t *testing.T) {
+	t.Parallel()
+
+	s, closeFn := testServer(t, func(w http.ResponseWriter, r *http.Request) error {
+		c, err := websocket.Accept(w, r, &websocket.AcceptOptions{
+			Subprotocols:       []string{"echo"},
+			InsecureSkipVerify: true,
+		})
+		if err != nil {
+			return err
+		}
+		defer c.Close(websocket.StatusInternalError, "")
+
+		err = wsecho.Loop(r.Context(), c)
+		if websocket.CloseStatus(err) != websocket.StatusNormalClosure {
+			return err
+		}
+		return nil
+	}, false)
+	defer closeFn()
+
+	wsURL := strings.Replace(s.URL, "http", "ws", 1)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*20)
+	defer cancel()
+
+	cmd := exec.CommandContext(ctx, "go", "test", "-exec=wasmbrowsertest", "./...")
+	cmd.Env = append(os.Environ(), "GOOS=js", "GOARCH=wasm", fmt.Sprintf("WS_ECHO_SERVER_URL=%v", wsURL))
+
+	b, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("wasm test binary failed: %v:\n%s", err, b)
+	}
+}
