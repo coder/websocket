9b9d7b4aac087688af34698a3a0b45fc8d6303a4
Anmol Sethi <hi@nhooyr.io>
2019-10-14 17:18:48 -0400

    Cleanup coveralls setup

--- ci/test.mk
+++ ci/test.mk
@@ -7,11 +7,15 @@ ci/out/coverage.html: gotest
 	go tool cover -html=ci/out/coverage.prof -o=ci/out/coverage.html
 
 coveralls: gotest
+	# https://github.com/coverallsapp/github-action/blob/master/src/run.ts
 	echo "--- coveralls"
-	export GIT_BRANCH=$${GITHUB_REF}
-	export BUILD_NUMBER=$${GITHUB_ACTION}
-	goveralls -coverprofile=ci/out/coverage.prof -service=github-actions
-
+	export GIT_BRANCH="$$GITHUB_REF"
+	export BUILD_NUMBER="$$GITHUB_SHA"
+	if [[ $$GITHUB_EVENT_NAME == pull_request ]]; then
+	  export CI_PULL_REQUEST="$$(jq .number "$$GITHUB_EVENT_PATH")"
+	  BUILD_NUMBER="$$BUILD_NUMBER-PR-$$CI_PULL_REQUEST"
+	fi
+	goveralls -coverprofile=ci/out/coverage.prof -service=github
 gotest:
 	go test -covermode=count -coverprofile=ci/out/coverage.prof -coverpkg=./... $${GOTESTFLAGS-} ./...
 	sed -i '/_stringer\.go/d' ci/out/coverage.prof
