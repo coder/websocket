e4fee52874b402afcb4cc7aa5cebddc393618800
Anmol Sethi <hi@nhooyr.io>
2021-01-09 07:30:29 -0500

    ci/test.sh: Work with BSD sed

--- ci/test.sh
+++ ci/test.sh
@@ -5,9 +5,9 @@ main() {
   cd "$(dirname "$0")/.."
 
   go test -timeout=30m -covermode=atomic -coverprofile=ci/out/coverage.prof -coverpkg=./... "$@" ./...
-  sed -i '/stringer\.go/d' ci/out/coverage.prof
-  sed -i '/nhooyr.io\/websocket\/internal\/test/d' ci/out/coverage.prof
-  sed -i '/examples/d' ci/out/coverage.prof
+  sed -i.bak '/stringer\.go/d' ci/out/coverage.prof
+  sed -i.bak '/nhooyr.io\/websocket\/internal\/test/d' ci/out/coverage.prof
+  sed -i.bak '/examples/d' ci/out/coverage.prof
 
   # Last line is the total coverage.
   go tool cover -func ci/out/coverage.prof | tail -n1
