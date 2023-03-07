54809d605a9cd025bcd7336308ec0ec00c4b879b
Gus Eggert <gus@gus.dev>
2023-03-07 09:51:46 -0500

    Update err message when dial ctx is nil
    Co-authored-by: Anmol Sethi <hi@nhooyr.io>

--- dial.go
+++ dial.go
@@ -159,7 +159,7 @@ func handshakeRequest(ctx context.Context, urls string, opts *DialOptions, copts
 
 	req, err := http.NewRequestWithContext(ctx, "GET", u.String(), nil)
 	if err != nil {
-		return nil, fmt.Errorf("failed to build HTTP request: %w", err)
+		return nil, fmt.Errorf("http.NewRequestWithContext failed: %w", err)
 	}
 	req.Header = opts.HTTPHeader.Clone()
 	req.Header.Set("Connection", "Upgrade")
