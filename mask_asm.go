f5397ae3d1bfdf120ce8a598a0b0b0fe2b86f784
Anmol Sethi <hi@nhooyr.io>
2023-10-19 07:48:47 -0700

    mask_asm.go: Disable AVX2
    Slower for some reason than just SIMD.
    
    Also no dependency on cpu package is nice.

--- mask_asm.go
+++ mask_asm.go
@@ -2,8 +2,6 @@
 
 package websocket
 
-import "golang.org/x/sys/cpu"
-
 func mask(key uint32, b []byte) uint32 {
 	if len(b) > 0 {
 		return maskAsm(&b[0], len(b), key)
@@ -11,7 +9,7 @@ func mask(key uint32, b []byte) uint32 {
 	return key
 }
 
-var useAVX2 = cpu.X86.HasAVX2 //lint:ignore U1000 mask_amd64.s
+var useAVX2 = false
 
 //go:noescape
 func maskAsm(b *byte, len int, key uint32) uint32
