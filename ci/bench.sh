ecf7dec40098cbc3b659d99b467c0d6c97f38c6c
Anmol Sethi <hi@nhooyr.io>
2023-10-19 19:52:36 -0700

    ci/bench.sh: Install QEMU on CI

--- ci/bench.sh
+++ ci/bench.sh
@@ -8,5 +8,10 @@ go test --run=^$ --bench=. --benchmem --memprofile ci/out/prof.mem --cpuprofile 
   go test --run=^$ --bench=. --benchmem --memprofile ../../ci/out/prof-thirdparty.mem --cpuprofile ../../ci/out/prof-thirdparty.cpu -o ../../ci/out/thirdparty.test "$@" .
 
   GOARCH=arm64 go test -c -o ../../ci/out/thirdparty-arm64.test .
+  if [ "${CI-}" ]; then
+	  sudo apt-get update
+	  sudo apt-get install -y qemu-user-static
+	  alias qemu-aarch64=qemu-aarch64-static
+  fi
   qemu-aarch64 ../../ci/out/thirdparty-arm64.test --test.run=^$ --test.bench=Benchmark_mask --test.benchmem --test.memprofile ../../ci/out/prof-thirdparty-arm64.mem --test.cpuprofile ../../ci/out/prof-thirdparty-arm64.cpu .
 )
