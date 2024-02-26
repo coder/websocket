4c273310109b8d134d96b35d66d7231e8c54a05b
Grigorii Khvatskii <gkhvatsk@nd.edu>
2024-02-26 16:12:18 -0500

    Fix unaligned load error on 32-bit architectures
    On some 32-bit architectures, 64-bit atomic operations panic when the
    value is not aligned properly.
    
    In this package, this causes netConn operations to panic when compiling
    with GOARCH=386, since netConn does atomic operations with int64 values
    in the netConn struct (namely, with readExpired and writeExpired).
    
    This commit fixes this by moving readExpired and writeExpired to the
    beginning of the struct, which makes them properly aligned.

--- netconn.go
+++ netconn.go
@@ -94,22 +94,23 @@ func NetConn(ctx context.Context, c *Conn, msgType MessageType) net.Conn {
 }
 
 type netConn struct {
+	readExpired  int64
+	writeExpired int64
+
 	c       *Conn
 	msgType MessageType
 
-	writeTimer   *time.Timer
-	writeMu      *mu
-	writeExpired int64
-	writeCtx     context.Context
-	writeCancel  context.CancelFunc
+	writeTimer  *time.Timer
+	writeMu     *mu
+	writeCtx    context.Context
+	writeCancel context.CancelFunc
 
-	readTimer   *time.Timer
-	readMu      *mu
-	readExpired int64
-	readCtx     context.Context
-	readCancel  context.CancelFunc
-	readEOFed   bool
-	reader      io.Reader
+	readTimer  *time.Timer
+	readMu     *mu
+	readCtx    context.Context
+	readCancel context.CancelFunc
+	readEOFed  bool
+	reader     io.Reader
 }
 
 var _ net.Conn = &netConn{}
