fb3bc9ffc78eafe357595c1b64b2a8e293d71c80
Mathias Fredriksson <mafredri@gmail.com>
2025-03-14 15:08:36 +0000

    chore(ci): update tools

--- ci/fmt.sh
+++ ci/fmt.sh
@@ -2,8 +2,8 @@
 set -eu
 cd -- "$(dirname "$0")/.."
 
-# Pin golang.org/x/tools, the go.mod of v0.25.0 is incompatible with Go 1.19.
-X_TOOLS_VERSION=v0.24.0
+# Pin golang.org/x/tools, the go.mod of v0.31.0 is incompatible with Go 1.22.
+X_TOOLS_VERSION=v0.30.0
 
 go mod tidy
 (cd ./internal/thirdparty && go mod tidy)
